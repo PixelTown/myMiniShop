16a372cc7af518cfe8d59477de85e9a3869d71ac
YifangZhang <yifangzhang2009@gmail.com>
2015-08-18 23:28:16 -0500

    changed into 4 tabs: login, crafting, selling, shopInfo

---
+++ MiniShop/MiniShop/CraftingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CraftingViewController.h
+//  MiniShop
+//
+//  Created by Yifang Zhang on 8/18/15.
+//  Copyright (c) 2015 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CraftingViewController : UIViewController
+
+@end
